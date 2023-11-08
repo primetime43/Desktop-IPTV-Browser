fe6b6fbebb4de9e1e68cc1f99f325cfb47a682a0
primetime43 <12754111+primetime43@users.noreply.github.com>
2023-11-08 15:34:39 -0500

    Added M3U playlist search all channels
    - Added the ability for the universal search to search through all channels in all categories

--- X-IPTV/UniversalSearchList.xaml.cs
+++ X-IPTV/UniversalSearchList.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using static X_IPTV.XtreamCodes;
+using static X_IPTV.M3UPlaylist;
+using System.Collections;
 
 namespace X_IPTV
 {
@@ -34,16 +36,23 @@ namespace X_IPTV
             if (USearchTextBox != null)
             {
                 var filterText = USearchTextBox.Text.ToLower();
-                var filteredItems = this.model.SearchChannels(filterText);
 
-                // Clear the existing items and add the filtered items
-                model.MyListBoxItems.Clear();
-                foreach (var item in filteredItems)
+                IList currentList = Instance.XtreamCodesChecked
+                    ? (IList)model.MyXtreamListBoxItems
+                    : (IList)model.MyM3uListBoxItems;
+
+                currentList.Clear();
+                var searchResult = Instance.XtreamCodesChecked
+                    ? model.SearchXtreamChannels(filterText).Cast<object>()
+                    : model.SearchM3uChannels(filterText).Cast<object>();
+
+                foreach (var item in searchResult)
                 {
-                    model.MyListBoxItems.Add(item);
+                    currentList.Add(item);
                 }
 
-                USearchChannelLst.ItemsSource = model.MyListBoxItems; // The UI will now update to show only filtered items
+                // The UI will now update to show only filtered items
+                USearchChannelLst.ItemsSource = currentList;
             }
         }
 
@@ -72,6 +81,23 @@ namespace X_IPTV
                     }
                 }
             }
+            else if(e.AddedItems.Count > 0 && Instance.M3uChecked)
+            {
+                M3UChannel m3uChannel = e.AddedItems[0] as M3UChannel;
+
+                if (m3uChannel != null)
+                {
+                    // Set the tempChannel property of the ChannelOptions instance.
+                    channelOp.tempChannel = m3uChannel;
+
+                    // Now display the selected channel data in the ChannelOptions window.
+                    if (channelOp.DisplaySelectedChannelData(m3uChannel))
+                    {
+                        // If the data is successfully displayed, show the ChannelOptions window.
+                        channelOp.Show();
+                    }
+                }
+            }
         }
 
         private void listBox1_MouseDown(object sender, RoutedEventArgs e)
@@ -86,26 +112,23 @@ namespace X_IPTV
                 }
             }*/
         }
-
-        //might be able to remove this
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            CategoryNav categoryNavWindow = new CategoryNav();
-            categoryNavWindow.Show();
-        }
     }
 
     public class UniversalSearchModel
     {
         private bool isInitialized = false;
-        private List<XtreamChannel> allChannels;
+        private List<XtreamChannel> allXtreamChannels;
+        private List<M3UChannel> allM3uChannels;
 
-        public ObservableCollection<XtreamChannel> MyListBoxItems { get; set; }
+        public ObservableCollection<XtreamChannel> MyXtreamListBoxItems { get; set; }
+        public ObservableCollection<M3UChannel> MyM3uListBoxItems { get; set; }
 
         public UniversalSearchModel()
         {
-            MyListBoxItems = new ObservableCollection<XtreamChannel>();
-            allChannels = new List<XtreamChannel>();
+            MyXtreamListBoxItems = new ObservableCollection<XtreamChannel>();
+            MyM3uListBoxItems = new ObservableCollection<M3UChannel>();
+            allXtreamChannels = new List<XtreamChannel>();
+            allM3uChannels = new List<M3UChannel>();
         }
 
 
@@ -115,25 +138,47 @@ namespace X_IPTV
 
             if (Instance.XtreamCodesChecked)
             {
-                allChannels.AddRange(Instance.XtreamChannels); // Load all channels, but do not add to MyListBoxItems
+                allXtreamChannels.AddRange(Instance.XtreamChannels); // Load all channels, but do not add to MyListBoxItems
             }
-            /*else if(Instance.M3uChecked)
+            else if(Instance.M3uChecked)
             {
-                allChannels.AddRange(Instance.M3UChannels); // Load all channels, but do not add to MyListBoxItems
-            }*/
+                allM3uChannels.AddRange(Instance.M3UChannels); // Load all channels, but do not add to MyListBoxItems
+            }
             isInitialized = true;
         }
 
-        public IEnumerable<XtreamChannel> SearchChannels(string filterText)
+        public IEnumerable<XtreamChannel> SearchXtreamChannels(string filterText)
         {
-            if (allChannels == null)
+            // Return an empty list if allXtreamChannels is null
+            if (allXtreamChannels == null)
             {
-                return Enumerable.Empty<XtreamChannel>(); // Return an empty list if allChannels is null
+                return Enumerable.Empty<XtreamChannel>();
             }
 
-            return allChannels
-                .Where(channel => (channel.ChannelName?.ToLower().Contains(filterText) ?? false) ||
-                                  (channel.EPGData?.Description.ToLower().Contains(filterText) ?? false));
+            // Use StringComparison.OrdinalIgnoreCase for a case-insensitive comparison that is also faster than converting strings to lower case.
+            var comparisonType = StringComparison.OrdinalIgnoreCase;
+
+            return allXtreamChannels
+                .Where(channel => channel != null && // Check that the channel is not null
+                    (channel.ChannelName != null && channel.ChannelName.IndexOf(filterText, comparisonType) >= 0) ||
+                    (channel.EPGData != null && channel.EPGData.Description != null && channel.EPGData.Description.IndexOf(filterText, comparisonType) >= 0));
+        }
+
+        public IEnumerable<M3UChannel> SearchM3uChannels(string filterText)
+        {
+            // Return an empty list if allM3uChannels is null
+            if (allM3uChannels == null)
+            {
+                return Enumerable.Empty<M3UChannel>();
+            }
+
+            // Use StringComparison.OrdinalIgnoreCase for a case-insensitive comparison that is also faster than converting strings to lower case.
+            var comparisonType = StringComparison.OrdinalIgnoreCase;
+
+            return allM3uChannels
+                .Where(channel => channel != null && // Check that the channel is not null
+                    (channel.ChannelName != null && channel.ChannelName.IndexOf(filterText, comparisonType) >= 0) ||
+                    (channel.EPGData != null && channel.EPGData.Description != null && channel.EPGData.Description.IndexOf(filterText, comparisonType) >= 0));
         }
     }
     public class UniversalSearchMyMockClass
