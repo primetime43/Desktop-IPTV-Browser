d420616da55928585597464dfdfe46c0ea9224d7
primetime43 <12754111+primetime43@users.noreply.github.com>
2024-11-23 19:22:35 -0500

    .net 9 FluentUI redesign rewrite
    - First commit of the project rewrite with .net 9 & FluentUI

---
+++ Desktop-IPTV-Browser/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Desktop_IPTV_Browser
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private DataTemplate _selectedPageTemplate;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            SelectedPage = "HomeTemplate"; // Set the default page
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _selectedPage;
+        public string SelectedPage
+        {
+            get => _selectedPage;
+            set
+            {
+                _selectedPage = value;
+                OnPropertyChanged(nameof(SelectedPage));
+                UpdateSelectedPageTemplate();
+            }
+        }
+
+        public DataTemplate SelectedPageTemplate
+        {
+            get => _selectedPageTemplate;
+            set
+            {
+                _selectedPageTemplate = value;
+                OnPropertyChanged(nameof(SelectedPageTemplate));
+            }
+        }
+
+        private void UpdateSelectedPageTemplate()
+        {
+            // Update the template dynamically based on SelectedPage
+            SelectedPageTemplate = FindResource(SelectedPage) as DataTemplate;
+        }
+    }
+}
